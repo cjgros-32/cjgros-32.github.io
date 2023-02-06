2ac3f8eead7ddb0fb854da826f212bd7193adc1a
christian gros <cjggros@gmail.com>
2023-02-06 00:49:35 -0300

    segunda escritura en el repositorio

--- funcion.js
+++ funcion.js
@@ -25,11 +25,21 @@ boton_ir_arriba.addEventListener("click", arriba)
 
 // Evento de validación para formulario //
 
-function validacion_formulario() {
-  let x = document.form["formulario"]["nombre"];
-  if (x == "") {
-    alert("por favor ingrese su nombre, apellido y mail");
+function validar_formulario() {
+  let nombre01 = document.forms["formulario"]["nombre"].value;
+  let apellido02 = document.forms["formulario"]["apellido"].value;
+  let mail03= document.forms["formulario"]["mail"].value;
+  
+  if (nombre01 == "") {
+    alert("POR FAVOR INGRESE SU NOMBRE");
+    return false;
+  }
+  if (apellido02 == "") {
+    alert("POR FAVOR INGRESE SU APELLIDO");
+    return false;
+  }
+  if (mail03 == "") {
+    alert("POR FAVOR INGRESE SU MAIL");    
     return false;
   }
 }
-
